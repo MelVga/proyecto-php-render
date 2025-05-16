cc51175f49a16290481e459d3d23908fade3bab8
Melissa Vega <melvega@MacBook-Air-de-Melissa.local>
2025-05-16 16:12:51 -0600

    cambios 7

--- api/conexion.php
+++ api/conexion.php
@@ -1,15 +1,13 @@
 <?php
-$host = "dpg-d0jp2ll6ubrc73amgba0-a.oregon-postgres.render.com";
-$port = "5432";
-$dbname = "bd_tareas";
-$user = "bd_tareas_user";
-$password = "UQiXp3T5SUPWPUeI25Vc5JVIJDIDZ7bt";
+$host = getenv("dpg-d0jp2ll6ubrc73amgba0-a.oregon-postgres.render.com");
+$dbname = getenv("bd_tareas");
+$user = getenv("bd_tareas_user");
+$password = getenv("UQiXp3T5SUPWPUeI25Vc5JVIJDIDZ7bt");
 
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
-
-if (!$conn) {
-    http_response_code(500);
-    echo json_encode(["error" => "Error de conexión: " . pg_last_error()]);
-    exit();
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
 }
-?>
